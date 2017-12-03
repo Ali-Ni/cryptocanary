157c8d88da8f46f8a30507572ba0ae54689543be
Ali Nikseresht <ali.nikseresht21@gmail.com>
2017-12-02 21:26:30 -0500

    Working sentiment analysis

--- canary/analysis.py
+++ canary/analysis.py
@@ -10,16 +10,25 @@ class Analysis:
     """Tweet analysis"""
     def __init__(self):
         self.client = language.LanguageServiceClient()
-        self.numtweets = 0
+        self.sum = 0
     def get_sentiment(self, text):
         """Use google cloud NLP to retrieve the sentiment of the text"""
+        print("Getting sentiment value...")
         document = types.Document(
             content=text,
             type=enums.Document.Type.PLAIN_TEXT)
         return self.client.analyze_sentiment(document=document).document_sentiment
-    def get_volume_factor(self):
-        """Return a factor from 0-1 dependant on the volume of tweets"""
-        return math.erf(VOLUME_FACTOR*self.numtweets)
+    def process_tweet(self, tweet):
+        """Process a tweet and add its weighted value to the sum of sentiments"""
+        sentiment = self.get_sentiment(tweet.text)
+        self.sum += sentiment.score*sentiment.magnitude*get_influence(tweet.followers)
+    def get_value(self):
+        """Return a net sentiment value between -1 and 1"""
+        return math.erf(VOLUME_FACTOR*self.sum)
+    def soft_reset(self):
+        """Soft reset of value"""
+        self.sum /= 2
+
 def get_influence(numfollowers):
     """Calculate an influence value (from 0-1) from the number of followers using a sigmoid function"""
-    return math.erf(INFLUENCE_ERROR_FACTOR*(numfollowers-FOLLOWER_LOWER_LIMIT))
+    return math.erf(INFLUENCE_ERROR_FACTOR*numfollowers)
--- main.py
+++
@@ -1,7 +0,0 @@
-from canary.analysis import Analysis
-
-analyzer = Analysis()
-
-out = analyzer.get_sentiment("I love memes!")
-print(out.magnitude)
-print(out.score)
